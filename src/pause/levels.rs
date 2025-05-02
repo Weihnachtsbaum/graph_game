ebda858a58fab6d849ac6b4b4d24ae6d65e94dc1
Weihnachtsbaum <140716472+Weihnachtsbaum@users.noreply.github.com>
2025-05-02 23:46:49 +0200

    Add pages to level select screen

--- src/pause/levels.rs
+++ src/pause/levels.rs
@@ -8,7 +8,11 @@ use crate::{
 };
 
 pub fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(GameState::LevelSelect), setup);
+    app.add_systems(
+        Update,
+        handle_arrows.run_if(in_state(GameState::LevelSelect)),
+    )
+    .add_systems(OnEnter(GameState::LevelSelect), setup);
 }
 
 #[derive(Component)]
@@ -92,6 +96,35 @@ fn setup(
                 ));
             }
         });
+}
+
+fn handle_arrows(
+    kb: Res<ButtonInput<KeyCode>>,
+    mut level_vertex_q: Query<(&mut LevelVertex, &MeshMaterial2d<VertexMaterial>, &Children)>,
+    mut text_q: Query<(&mut Text2d, &mut TextColor)>,
+    mut vertex_materials: ResMut<Assets<VertexMaterial>>,
+    level: Res<Level>,
+) -> Result {
+    let dir = if kb.just_pressed(KeyCode::ArrowLeft) {
+        -1
+    } else if kb.just_pressed(KeyCode::ArrowRight) {
+        1
+    } else {
+        return Ok(());
+    };
+    for (mut level_vertex, mesh_material, children) in &mut level_vertex_q {
+        if level_vertex.0 <= 10 && dir < 0 {
+            return Ok(());
+        }
+        level_vertex.0 = (level_vertex.0 as i32 + dir * 10) as u64;
+        let (mut text, mut color) = text_q.get_mut(children[0])?;
+        text.0 = format!("{}", level_vertex.0);
+        let material = vertex_materials
+            .get_mut(mesh_material)
+            .ok_or("Invalid vertex material handle")?;
+        material.set_solved(level_vertex.0 < level.0, &mut color);
+    }
+    Ok(())
 }
 
 #[allow(clippy::type_complexity)]
